656adfa3937a80cfc4445f8bae30f5c4bb47b4e8
Siphiwe <nompychiloane@gmail.com>
2023-04-25 13:52:25 -0700

    Free a pointer

---
+++ string.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string to be checked
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - lexicogarphic comparison of two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: - if s1 < s2, + if s1 > s2, 0 if s1 == s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string search
+ * @needle: substring to find
+ * Return: address of next char or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination buffer
+ * @src: source buffer
+ * Return: pointer to destination buffer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
